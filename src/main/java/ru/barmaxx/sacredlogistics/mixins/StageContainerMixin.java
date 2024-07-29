bdd9a509cc4f635c7d948b64b5bcdc1cdbe273d8
HollowHorizon <artemm@xaker.ru>
2024-07-29 15:39:28 +0300

    ore stages integration

---
+++ src/main/java/ru/barmaxx/sacredlogistics/mixins/StageContainerMixin.java
@@ -0,0 +1,20 @@
+package ru.barmaxx.sacredlogistics.mixins;
+
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.level.block.Blocks;
+import net.sixik.orestages.StageContainer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import ru.barmaxx.sacredlogistics.registry.SacredBlocks;
+
+@Mixin(value = StageContainer.class)
+public class StageContainerMixin {
+    @Inject(at = @At("TAIL"), method = "apply(Ljava/lang/Void;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", remap = false)
+    private void onApply(Void p_10793_, ResourceManager p_10794_, ProfilerFiller p_10795_, CallbackInfo ci) {
+        StageContainer.getOrCreateBlock("demonite_and_crimson", SacredBlocks.INSTANCE.getDEMONITE_ORE().get().defaultBlockState(), null, Blocks.NETHERRACK.defaultBlockState(), false);
+        StageContainer.getOrCreateBlock("demonite_and_crimson", SacredBlocks.INSTANCE.getCRIMSON_ORE().get().defaultBlockState(), null, Blocks.NETHERRACK.defaultBlockState(), false);
+    }
+}
--- src/main/kotlin/ru/barmaxx/sacredlogistics/events/SacredEvents.kt
+++ src/main/kotlin/ru/barmaxx/sacredlogistics/events/SacredEvents.kt
@@ -1,17 +1,33 @@
 package ru.barmaxx.sacredlogistics.events
 
+import com.sun.source.util.SourcePositions
+import net.darkhax.gamestages.GameStageHelper
 import net.minecraft.network.chat.Component
+import net.minecraft.server.level.ServerPlayer
 import net.minecraft.world.entity.player.Player
 import net.minecraftforge.event.entity.living.LivingDeathEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraftforge.registries.ForgeRegistries
 
 object SacredEvents {
     @SubscribeEvent
     fun onEntityKilled(event: LivingDeathEvent) {
         event.source.entity?.let { source ->
             if (source is Player) {
+                val entity = event.entity
+                val type = ForgeRegistries.ENTITY_TYPES.getKey(entity.type)?.toString() ?: return@let
                 source.sendSystemMessage(Component.literal("You killed ${event.entity.name.string}"))
+
+                if(type.equals("minecraft:zombie")) {
+                    GameStageHelper.addStage(source as? ServerPlayer ?: return, "demonite_and_crimson")
+                }
             }
         }
     }
 }
+
+val ORE_STAGES = mapOf(
+    "minecraft:wither" to listOf("demonite_ore", "bagryan_ore"),
+    "ice_and_fire:dragon" to listOf("chlorofil_ore"),
+    "minecraft:ender_dragon" to listOf("cobalt_ore")
+)
